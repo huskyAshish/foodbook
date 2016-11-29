23dee3c5f011f2213b94701cd0942a4e556bf53c
huskyAshish <kalbhor.a@husky.neu.edu>
2016-11-28 21:05:18 -0500

    User controller added.

---
+++ project/client/views/user/user.controller.client.js
@@ -0,0 +1,88 @@
+(function () {
+    angular
+        .module("Foodbook")
+        .controller("LoginController", LoginController)
+        .controller("RegisterController", RegisterController)
+        .controller("ProfileController", ProfileController);
+
+    function LoginController($location, UserService) {
+        var vm = this;
+        vm.login = login;
+
+        function login(user) {
+
+            if(user == null)
+                vm.error = "Please enter username and password!"
+
+            UserService
+                .findUserByCredentials(user.username, user.password)
+                .success(function(user){
+                    if(user === '0') {
+                        vm.error = "Unable to login!";
+                    } else {
+                        $location.url("/user/" + user._id);
+                    }
+                })
+                .error(function () {
+
+                });
+        }
+    }
+
+    function RegisterController($location, UserService) {
+        var vm = this;
+        vm.register = register;
+
+        function register(user) {
+            if(user == null || user.username == null){
+                vm.error = "Enter valid name !";
+            }else{
+
+                if(user.password == user.verify){
+                    UserService
+                        .createUser(user)
+                        .success(function (user) {
+                            $location.url("/user/" + user._id);
+                        })
+                        .error(function () {
+
+                        });
+
+                }else{
+                    vm.error = "Passwords do not match !"
+                }
+            }
+        }
+    }
+
+    function ProfileController($routeParams, UserService) {
+        var vm = this;
+        var userId = $routeParams['uid'];
+
+        UserService
+            .findUserById(userId)
+            .success(function(user){
+                if(user != '0'){
+                    vm.user = user;
+                }
+            })
+            .error(function () {
+
+            });
+
+        vm.updateUser = updateUser;
+
+        function updateUser(user) {
+            UserService
+                .updateUser(userId, user)
+                .success(function(user){
+
+                })
+                .error(function () {
+
+                });
+        }
+
+    }
+
+})();
