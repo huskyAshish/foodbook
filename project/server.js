a7bac64186a47e2575bdbb2c890b0472cc07a176
huskyAshish <kalbhor.a@husky.neu.edu>
2016-11-28 20:50:48 -0500

    Adding server.js

---
+++ project/server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(__dirname + '/public'));
+
+require ("./server/app")(app);
+
+app.set('ipaddress', (process.env.IP));
+app.set('port', (process.env.PORT || 5000));
+app.listen(app.get('port'), app.get('ipaddress'));
