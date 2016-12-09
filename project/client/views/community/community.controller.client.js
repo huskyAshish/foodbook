1ef2d4d3894bf8b40510bc77832fade4641d1b0d
surekha <surekha@ccs.neu.edu>
2016-12-09 14:23:40 -0800

    Stop following working

--- project/client/views/community/community.controller.client.js
+++ project/client/views/community/community.controller.client.js
@@ -65,69 +65,29 @@
                 .addFollowing(vm.currentUserId, following)
                 .then(
                     function (user) {
-                        vm.message = following.username + " added as friend!";
+                        vm.message = "Started following: " + following.username;
                         init();
                     },
                     function(err){
-                        vm.error = "Aww Snap! Some error occurred :( Could not follow " + following.username + "user.";
+                        vm.error = "Aww Snap! Some error occurred :( Could not follow " + following.username + "user."
+                        console.log(err);
                     }
-                )
+                );
         }
 
-        function removeFriend(fname){
-
-         /*   UserService.findUserByUsername(fname)
-                .then(function(userFriend){
-
-                    UserService.removeFriend($rootScope.currentUser._id,userFriend.data._id)
-                        .then(
-                            function(remvedList){
-                                vm.message= "Removed as friend";
-                                var friends = [];
-                                friends = $rootScope.currentUser.friends;
-                                friends.splice(friends.indexOf(userFriend.data._id),1);
-
-                                var updatedUser = {
-                                    username: $rootScope.currentUser.username,
-                                    password: $rootScope.currentUser.password,
-                                    firstName: $rootScope.currentUser.firstName,
-                                    lastName: $rootScope.currentUser.lastName,
-                                    email: $rootScope.currentUser.email,
-                                    phones: $rootScope.currentUser.phones,
-                                    review: $rootScope.currentUser.review,
-                                    likes: $rootScope.currentUser.likes,
-                                    friends : friends
-                                };
-
-                                UserService.updateUser( $rootScope.currentUser._id, updatedUser)
-                                    .then(
-                                        function (updatedUser){
-                                            if (updatedUser.data != null) {
-                                                UserService.setUser(updatedUser.data);
-                                            }
-                                            else
-                                            {
-                                                vm.message = "Cannot update User";
-                                            }
-                                        },
-                                        function (error){
-                                            vm.message = "Cannot update User";
-                                        });
-
-                                UserService.findFriends($rootScope.currentUser._id)
-                                    .then(function(users){
-                                        vm.friends = users.data;
-                                    },function(err){
-
-                                    });
-
-                            },function(err){
-
-                            }
-                        )
-
-                });*/
-
+        function removeFriend(following){
+            UserService
+                .removeFollowing(vm.currentUserId, following._id)
+                .then(
+                    function (user) {
+                        vm.message = "Stopped following: " + following.username;
+                        init();
+                    },
+                    function (err) {
+                        vm.error = "Aww Snap! Some error occurred :( Couldn't stop following " + following.username + "user."
+                        console.log(err);
+                    }
+                );
         }
     }
 })();
