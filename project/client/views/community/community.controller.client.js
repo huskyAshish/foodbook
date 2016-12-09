a4a3cb36d8c51619390231e2a8c0dc53a7447c09
surekha <surekha@ccs.neu.edu>
2016-12-09 15:06:08 -0800

    Refactored code to remove $rootScope

--- project/client/views/community/community.controller.client.js
+++ project/client/views/community/community.controller.client.js
@@ -3,60 +3,55 @@
         .module("FoodbookApp")
         .controller("CommunityController", CommunityController);
 
-    function CommunityController($location, $rootScope, UserService) {
+    function CommunityController(UserService) {
         var vm = this;
 
-        /*function init() {
+        vm.addFollowing = addFollowing;
+        vm.removeFriend = removeFriend;
+
+        function init() {
+
             UserService
                 .findCurrentUser()
                 .success(function (user) {
                     if(user !== '0'){
                         vm.currentUser = user;
                         vm.currentUserId = user._id;
+
+                        UserService
+                            .findSuggestedUsers(vm.currentUserId)
+                            .then(
+                                function (users) {
+                                    vm.suggestedUsers = users.data;
+                                },
+                                function (err) {
+                                    console.log(err);
+                                });
+
+                        UserService
+                            .findFollowing(vm.currentUserId)
+                            .then(
+                                function (following) {
+                                    vm.following = following.data;
+                                },
+                                function (err) {
+                                    console.log(err);
+                                });
+
+                        UserService
+                            .findFollowers(vm.currentUserId)
+                            .then(
+                                function (followers) {
+                                    vm.followers = followers.data;
+                                },
+                                function (err) {
+                                    console.log(err);
+                                });
                     }
                 })
                 .error(function (error) {
                     console.log(error);
                 });
-        }
-        init();*/
-
-        vm.currentUser = $rootScope.currentUser;
-        vm.currentUserId = vm.currentUser._id;
-
-        vm.addFollowing = addFollowing;
-        vm.removeFriend = removeFriend;
-
-        function init() {
-            UserService
-                .findSuggestedUsers(vm.currentUserId)
-                .then(
-                    function (users) {
-                        vm.suggestedUsers = users.data;
-                    },
-                    function (err) {
-                        console.log(err);
-                    });
-
-            UserService
-                .findFollowing(vm.currentUserId)
-                .then(
-                    function (following) {
-                        vm.following = following.data;
-                    },
-                    function (err) {
-                        console.log(err);
-                    });
-
-            UserService
-                .findFollowers(vm.currentUserId)
-                .then(
-                    function (followers) {
-                        vm.followers = followers.data;
-                    },
-                    function (err) {
-                        console.log(err);
-                    });
         }
         init();
 
