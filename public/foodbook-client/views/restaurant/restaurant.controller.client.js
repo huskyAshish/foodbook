e2ae4a428fb4c2baa333bbc6a3d143b378830729
surekha <surekha@ccs.neu.edu>
2016-12-14 01:17:21 -0800

    Revert "Force SSL for website"
    This reverts commit f25d07af655a732e2898e10c2d42edb9ff3f48dd.

--- public/foodbook-client/views/restaurant/restaurant.controller.client.js
+++ public/foodbook-client/views/restaurant/restaurant.controller.client.js
@@ -40,11 +40,6 @@
                         console.log(err);
                     }
                 );
-
-            if ($location.protocol() !== 'https' && window.location.href.indexOf('localhost') === -1) {
-                $window.location.href = $location.absUrl().replace('http', 'https');
-                console.log($window.location.href);
-            }
         }
 
         init();
