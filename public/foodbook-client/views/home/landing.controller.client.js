39dbbfde62893d3da3236f0568f6113f6ca32290
Akash <singh.aka@husky.neu.edu>
2016-12-14 01:27:15 -0800

    Correcting formatting

--- public/foodbook-client/views/home/landing.controller.client.js
+++ public/foodbook-client/views/home/landing.controller.client.js
@@ -67,17 +67,13 @@
             if (!(vm.searchLocation == undefined || vm.searchLocation == null || vm.searchLocation == "")) {
                 vm.search();
             }
-        }
-        init();
-
-        function forceSSL() {
             if ($location.protocol() !== 'https' && window.location.href.indexOf('localhost') === -1) {
                 $window.location.href = $location.absUrl().replace('http', 'https');
                 console.log($window.location.href);
             }
         }
-        forceSSL();
-
+        init();
+        
         function search() {
             SearchService
                 .getSearchResults(vm.searchLocation, vm.searchKeyword)
